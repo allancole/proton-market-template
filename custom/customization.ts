5c7f549b3a1886eeb5254db918064921ac8e900e
Allan Cole <allan.cole@gmail.com>
2022-01-01 16:12:44 -0500

    Fix compiling error.

--- custom/customization.ts
+++ custom/customization.ts
@@ -302,6 +302,7 @@ export interface FeaturedSectionProps {
     color: string;
   };
   backgroundColor: string;
+  paddingTop: string;
   carouselButtonsBackgroundColor: string;
   carouselButtonsBorderColor: string;
 }
